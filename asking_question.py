10bbd6cb9c6cb9d19bbd7e98a2d26f54f25be9a7
arianshakoo <ariyanshakoo832@gmail.com>
2021-10-15 15:14:13 +0330

    Create asking_question.py

---
+++ asking_question.py
@@ -0,0 +1,30 @@
+from functions import speak
+from functions import get_audio
+#question 1
+text = get_audio()
+if "how are you" in text:
+	speak("fine thanks what about you?")
+	text = get_audio()
+	if text== "good" or text== "perfect" or text== "not bad" or text== "very good":
+		speak("thats perfect!")
+	else:
+		speak("do you want to make you happy?")
+		text = get_audio()
+		if "yes" in text or "yea" in text or "yeap" in text or "yeah" in text:
+			speak("i dont have any jokes to tell you. ha. ha ha. ha")
+#question 2
+elif text == "what's my name" or text == "do you know my name" or text == "what is my name" or "what's my name" in text:
+	speak("dont know do you want to tell that?")
+	text = get_audio()
+	if "yes" in text or "yea" in text or "yeap" in text or "yeah" in text:
+		speak("so tell that")
+		text = get_audio()
+		if "my name is" in text or "its" in text:
+			speak("i cant uderstand that please just tell your name")
+		text = get_audio()
+		speak("hello")
+		speak(text)
+	elif "no" in text:
+		speak("hahaha")
+	else: 
+		speak("please ask 1 question and answer my question")
