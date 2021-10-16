0df481c7fcb1c19b04cb6bec53978323d23db30e
arianshakoo <88421447+arianshakoo@users.noreply.github.com>
2021-10-16 17:21:50 +0330

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,26 +1,3 @@
-import os
-import time
-import speech_recognition as sr
-from gtts import gTTS
-import playsound
-
-def speak(text):
-	tts = gTTS(text=text,lang="en")
-	filename = "C:/Users/USER/AppData/Local/Programs/Python/Python39/voice.mp3"
-	tts.save(filename)
-	playsound.playsound(filename)
-
-def get_audio():
-	r = sr.Recognizer()
-	with sr.Microphone() as source :
-		audio = r.listen(source)
-		said = ""
-		try:
-			said = r.recognize_google(audio)
-			print(said)
-		except Exception as e:
-			print("Exeption :" + str(e))
-	return said
 from functions import speak
 from functions import get_audio
 #question 1
